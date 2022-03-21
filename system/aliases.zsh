120b20a37142ce3a41a200f6bda7a54e29ed87bc
Tomasz Cielecki <tomasz@ostebaronen.dk>
2022-03-21 22:19:24 +0100

    Add some aliases

---
+++ system/aliases.zsh
@@ -0,0 +1,8 @@
+alias top="top -o cpu"
+
+alias l="ls -lAh --color=auto"
+alias ll="ls -l --color=auto"
+alias la="ls -A --color=auto"
+
+alias nukebinobj="find . -type d -name bin -o -name obj | xargs rm -r > /dev/null"
+alias updatelocate="sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist"
