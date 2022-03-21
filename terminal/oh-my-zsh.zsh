7c1336e9d88fa66c44e3e64e04f30e3c5123acd5
Tomasz Cielecki <tomasz@ostebaronen.dk>
2022-03-21 22:18:50 +0100

    setup powerlevel10k theme

---
+++ terminal/oh-my-zsh.zsh
@@ -0,0 +1,8 @@
+unsetopt SHARE_HISTORY
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
