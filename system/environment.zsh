3f9216bfc73c63dc8f327cd5a6ed9036eefa7fc2
Tomasz Cielecki <tomasz@trackman.com>
2022-06-08 07:52:57 +0200

    update system files

---
+++ system/environment.zsh
@@ -0,0 +1,12 @@
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
---
+++ system/path.zsh
@@ -0,0 +1,5 @@
+export ANDROID_SDK="/Users/tomaszcielecki/Library/Developer/Xamarin/android-sdk-macosx/"
+export PATH="$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$PATH"
+
+export DOTNET_TOOLS="/Users/tomaszcielecki/.dotnet/tools"
+export PATH="$DOTNET_TOOLS:$PATH"
