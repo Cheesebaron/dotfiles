181e13825a15c38efe9cb451b67f8dbfab7ef977
Tomasz Cielecki <tci@trackman.com>
2022-07-06 08:46:35 +0200

    remove ssh symlink, using https + git-credentials-manager-core now

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 export ANDROID_SDK="/Users/tomaszcielecki/Library/Developer/Xamarin/android-sdk-macosx/"
-export PATH="$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$PATH"
+export PATH="$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$PATH:$ANDROID_SDK/cmdline-tools/latest/bin"
 
 export DOTNET_TOOLS="/Users/tomaszcielecki/.dotnet/tools"
 export PATH="$DOTNET_TOOLS:$PATH"
