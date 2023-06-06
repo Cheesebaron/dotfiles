fbf5aaa61c2b8d23f3fd897545a83397ce41081e
Tomasz Cielecki <tci@trackman.com>
2023-06-06 19:54:38 +0200

    Add ruby to path

--- system/path.zsh
+++ system/path.zsh
@@ -4,3 +4,5 @@ export PATH="$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$PATH:$ANDROID_SDK/c
 
 export DOTNET_TOOLS="/Users/tomaszcielecki/.dotnet/tools"
 export PATH="$DOTNET_TOOLS:$PATH"
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+export PATH="/opt/homebrew/lib/ruby/gems/3.1.0/bin:$PATH"
